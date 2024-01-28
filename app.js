d20d11026367ea18e7e48fb725ffd924985464e4
Syahril Akbar <89430734+syahrilakbar63@users.noreply.github.com>
2024-01-28 06:50:47 +0000

    Update generationConfig in app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: MODEL_NAME });
 
 const generationConfig = {
-  temperature: 0.9,
-  topK: 1,
+  temperature: 0.5,
+  topK: 20,
   topP: 1,
   maxOutputTokens: 3000,
 };
